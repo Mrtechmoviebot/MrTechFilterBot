735b3a790ed85aa05a1462134510af50d91f468d
ledass <nistalieocs@gmail.com>
2025-08-03 21:05:45 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -10,6 +10,7 @@ from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
 from utils import get_settings, save_group_settings
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
